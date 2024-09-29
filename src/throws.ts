7598c4c1843096baf7cebff4b8c5e260af18075b
Thijs Koerselman <thijskoerselman@gmail.com>
2024-09-29 17:07:11 +0200

    Update comment

--- src/throws.ts
+++ src/throws.ts
@@ -1,9 +1,7 @@
 /**
- * Check if a value is present and throw an error if it doesn't. It checks only for
- * undefined or null, because when writing Typescript we typically only throw on those conditions.
- *
- * @todo Not sure if we'd like to check for empty strings. I don't see a typical
- *   use-case.
+ * Check if a value is present and throw an error if it doesn't. It checks only
+ * for undefined or null, because when writing Typescript we typically only
+ * throw on those conditions.
  */
 export function throws<T>(value: T | undefined, msg: string): asserts value {
   if (value === undefined || value === null) {
